46a560293b10f872ea4b89bcf532996a82251bf3
Diego Smarrini <diegots@gmail.com>
2026-07-16 15:51:57 -0300

    fix: configure Reverb authorization for Laravel tests

--- api/tests/TestCase.php
+++ api/tests/TestCase.php
@@ -11,9 +11,16 @@ abstract class TestCase extends BaseTestCase
         foreach ([
             'APP_ENV' => 'testing',
             'APP_KEY' => 'base64:bG9jYWwtZGV2LWtleS0zMi1ieXRlcy1sb25nISEhISE=',
+            'BROADCAST_CONNECTION' => 'reverb',
             'DB_CONNECTION' => 'sqlite',
             'DB_DATABASE' => ':memory:',
             'DB_URL' => '',
+            'REVERB_APP_ID' => 'testing-app',
+            'REVERB_APP_KEY' => 'testing-key',
+            'REVERB_APP_SECRET' => 'testing-secret',
+            'REVERB_HOST' => 'localhost',
+            'REVERB_PORT' => '8080',
+            'REVERB_SCHEME' => 'http',
         ] as $name => $value) {
             putenv($name.'='.$value);
             $_ENV[$name] = $value;
